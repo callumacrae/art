9a6f78b43be3d458b0060151ba0526cde3ccce65
Callum Macrae <callum@macr.ae>
2016-05-14 11:04:20 +0100

    Made twice as big

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ import random from './lib/random';
 
 export const NUMBER_OF_LINES = 1000;
 
-export const startLength = () => 200 + random() * 250;
-export const endLength = () => 400 + random() * 50;
+export const startLength = () => 400 + random() * 500;
+export const endLength = () => 800 + random() * 100;
 
 
 export const BACKGROUND_COLOR = '#262819';
@@ -19,10 +19,10 @@ export const COLORS = [
 	`rgba(142, 55, 48, ${opacity})` // red
 ];
 
-export const LINE_WIDTH = 2;
+export const LINE_WIDTH = 4;
 
 
 // Line generation config
-export const SEGMENT_LENGTH = 5;
+export const SEGMENT_LENGTH = 10;
 export const BIAS_TO_PERFECT = 0.5;
-export const RANDOM_FACTOR = 0.5;
+export const RANDOM_FACTOR = 1;
